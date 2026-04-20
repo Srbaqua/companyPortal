210b315b8f6cd78d13f7f4db837433148c484564
Srbaqua <23bcs101@nith.ac.in>
2026-04-20 22:13:57 +0530

    CORS solved

--- server/src/server.js
+++ server/src/server.js
@@ -9,12 +9,7 @@ const adminRoutes = require("./routes/admin");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
